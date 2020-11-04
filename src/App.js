e3bafcb3369eff15f6c53e908a90906fa4c86552
eugene.malin <jenia96@live.com>
2020-11-05 00:02:28 +0300

    Remove template files and correct structure

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
+import { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1>TIC TAC TOE</h1>
+        </header>
+        <body className='App-body'>
+          <div>Body</div>
+        </body>
+        <footer className='App-footer'>
+          <h6>Eugene Malin</h6>
+        </footer>
+      </div>
+    );
+  }
 }
 
 export default App;
