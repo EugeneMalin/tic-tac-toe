d44c72a4626cb08da74d2a2c7b036fca17d74c4d
eugene.malin <jenia96@live.com>
2020-11-08 16:19:19 +0300

    Add header to app

--- src/App.js
+++ src/App.js
@@ -1,15 +1,42 @@
 import { Component } from 'react';
 import './App.css';
+import { Header } from './components/Header';
+import { DEFAULT_COMPLEXITY, DEFAULT_MODE } from './Const';
+
+
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      complexity: DEFAULT_COMPLEXITY,
+      mode: DEFAULT_MODE
+    }
+
+    this.onComplexityChanged = this.onComplexityChanged.bind(this);
+    this.onModeChanged = this.onModeChanged.bind(this);
+  }
+
+  onComplexityChanged(e, complexity) {
+    this.setState({complexity});
+  }
+
+  onModeChanged(e, mode) {
+    this.setState({mode});
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <h1>TIC TAC TOE</h1>
-        </header>
+        <Header 
+          className='App-header'
+          complexity={this.state.complexity} 
+          mode={this.state.mode}
+          onComplexityChanged={this.onComplexityChanged}
+          onModeChanged={this.onModeChanged}
+        />
         <body className='App-body'>
-          <div>Body</div>
+          <div>Body {`${this.state.complexity} ${this.state.mode}`}</div>
         </body>
         <footer className='App-footer'>
           <h6>Eugene Malin</h6>
