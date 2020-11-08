d09bbec7de04d0f3572521c54406f2d1a3c8157f
eugene.malin <jenia96@live.com>
2020-11-08 15:41:41 +0300

    Add base const

---
+++ src/Const.js
@@ -0,0 +1,23 @@
+
+export const ITEM_HEIGHT = 48;
+
+export const ITEM_PADDING_TOP = 8;
+
+export const DEFAULT_MODE = 'signle';
+
+export const DEFAULT_COMPLEXITY = 'low';
+
+export const COMPLEXITY_LEVELS = [{
+        name: 'low'
+    }, {
+        name: 'medium'
+    }, {
+        name: 'high'
+    }
+]
+
+export const GAME_MODES = [{
+    name: 'signle'
+}, {
+    name: 'multi'
+}]
