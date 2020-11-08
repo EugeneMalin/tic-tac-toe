4e6534c5231c7cbfe8948bca0c657c873db86bed
eugene.malin <jenia96@live.com>
2020-11-09 00:38:05 +0300

    Add check for state

--- src/data/Field.js
+++ src/data/Field.js
@@ -1,8 +1,19 @@
+/**
+ * Класс для хранения и обработки состояния игрового поля
+ */
+
 import { Point } from "./Point"
 
+const MIN_FEILD_SIZE = 3;
+
+const DIRECTIONS_COUNT = 4;
+
 export class Field {
-    constructor(size = 3) {
-        this.points = []
+    constructor(size = MIN_FEILD_SIZE) {
+        this.points = [];
+
+        this.winLine = null;
+
         for (let i = 0; i < size; i++) {
             const row = []
             for (let j = 0; j < size; j++) {
@@ -10,6 +21,10 @@ export class Field {
             }
             this.points.push(row)
         }
+    }
+
+    isActive() {
+        return !this.winLine
     }
     
     map(callback) {
@@ -23,7 +38,64 @@ export class Field {
             throw new Error('Клетка уже использована');
         }
 
-        this.points[x][y].state = 'default';
-        this.points[x][y].type = type;
+        point.state = 'default';
+        point.type = type;
+
+        const [hasWin, winLine] = this._checkState(x, y);
+
+        if (hasWin) {
+            this.winLine = winLine;
+            winLine.forEach((pointer) => {
+                this.points[pointer[0]][pointer[1]].state = 'active'
+            })
+        }
+    }
+
+    _checkState(x, y) {
+        // пример обработки на минимальном размере
+        //     N          +   +   +
+        //   NW NE          + + +
+        // W       E      * * 0 + +
+        //   SW SE          * * *
+        //     S          *   *   *
+        // 0 - это точка соответствующая х и у
+
+        let [hasWinLine, winLine] = [false, []];
+
+        for (let dir = 0; dir < DIRECTIONS_COUNT; dir++) {
+            [hasWinLine, winLine] = this._checkLine(x, y, dir);
+            console.log('_________________')
+        }
+        return [hasWinLine, winLine];
+    }
+
+    _checkLine(x, y, direction) {
+        const point = this.points[x][y];
+        const lineSize = Math.min(MIN_FEILD_SIZE, this.points.length, this.points[0].length);
+        let pointsCount = 0;
+        let winLine = [];
+
+        for (let step = 0; step < lineSize * 2 - 1; step++) {
+            const directXStep = step * (direction === 0 || direction === 2 ? 1 : (direction === 1 ? 0 : -1));
+            const directYStep = step * (direction === 0 || direction === 1 ? 1 : (direction === 3 ? 0 : -1));
+
+            const newX = directXStep - x;
+            const newY = directYStep - y;
+            console.log(`[${newX}, ${newY}]`)
+
+            if (newX < 0 || newY < 0 || newX >= lineSize || newY >= lineSize) {
+                continue;
+            }
+
+            const pointCur = this.points[newX] && this.points[newX][newY]
+            if (pointCur && pointCur.type === point.type) {
+                pointsCount++;
+                winLine.push([newX, newY])
+            } else {
+                pointsCount = 0;
+                winLine = []
+            }
+        }
+        return [pointsCount >= lineSize - 1, winLine];
     }
 }
