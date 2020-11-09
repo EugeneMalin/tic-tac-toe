bceb5468ee4b5a748d4fb40db92b78d7b990e947
eugene.malin <jenia96@live.com>
2020-11-09 23:18:05 +0300

    Add units row count setting component

---
+++ src/components/RowSize.jsx
@@ -0,0 +1,40 @@
+/**
+ * Win line rowSize setting for field
+ */
+
+import { FIELD_ROW_SIZE, ITEM_HEIGHT, ITEM_PADDING_TOP, MIN_FEILD_SIZE } from "../Const";
+import { Input, InputLabel, Select, MenuItem, FormControl } from "@material-ui/core";
+
+const MenuProps = {
+    PaperProps: {
+        style: {
+            maxHeight: ITEM_HEIGHT * 4.5 + ITEM_PADDING_TOP,
+            width: 250,
+        }
+    }
+};
+
+export function RowSize(props) {
+    return (
+        <FormControl disabled={props.disabled || (!(props.size - MIN_FEILD_SIZE))} className={`RowSize ${props.className}`}>
+            <InputLabel className="RowSize-label" id="rowSize-label">Row size</InputLabel>
+            <Select
+                labelId="rowSize-label"
+                id="rowSize"
+                className="RowSize-value"
+                onChange={(e, item) => {
+                    props.onRowSizeChanged(e, item.props.value, item.props.rowSize)
+                }}
+                MenuProps={MenuProps}
+                input={<Input />}
+                value={props.rowSize || MIN_FEILD_SIZE}
+            >
+                {FIELD_ROW_SIZE.slice(0, props.size - MIN_FEILD_SIZE + 1).map((value) => (
+                    <MenuItem key={value} value={value}>
+                    {`${value} units`}
+                    </MenuItem>
+                ))}
+            </Select>
+        </FormControl>
+    )
+}
